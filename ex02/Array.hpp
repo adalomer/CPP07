b30942633ddb12729f91844d548cb35768040aaa
Omer Ali Adali <omadali@k2m16s03.42kocaeli.com.tr>
2026-05-05 20:10:46 +0300

    update ex03

---
+++ ex02/Array.hpp
@@ -0,0 +1,51 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <stdexcept>
+
+template <typename T>
+class Array {
+private:
+    T* _data;
+    unsigned int _size;
+
+public:
+    Array() : _data(NULL), _size(0) {}
+
+    Array(unsigned int n) : _data(new T[n]()), _size(n) {}
+
+    Array(const Array& other) : _data(NULL), _size(0) {
+        *this = other;
+    }
+
+    Array& operator=(const Array& other) {
+        if (this != &other) {
+            T* tmp = (other._size > 0) ? new T[other._size] : NULL;
+            for (unsigned int i = 0; i < other._size; ++i) {
+                tmp[i] = other._data[i];
+            }
+            delete[] _data;
+            _data = tmp;
+            _size = other._size;
+        }
+        return *this;
+    }
+
+    ~Array() { delete[] _data; }
+
+    T& operator[](unsigned int index) {
+        if (index >= _size)
+            throw std::out_of_range("Array index out of range");
+        return _data[index];
+    }
+
+    const T& operator[](unsigned int index) const {
+        if (index >= _size)
+            throw std::out_of_range("Array index out of range");
+        return _data[index];
+    }
+
+    unsigned int size() const { return _size; }
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Array.hpp"
+
+int main() {
+	Array<int> a(5);
+	for (unsigned int i = 0; i < a.size(); ++i)
+		a[i] = static_cast<int>(i * 10);
+
+	for (unsigned int i = 0; i < a.size(); ++i)
+		std::cout << a[i] << " ";
+	std::cout << std::endl;
+
+	try {
+		std::cout << a[10] << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
