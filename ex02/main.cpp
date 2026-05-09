c2c286f94a962ea5b9f33d2fbdd1dd279bc54afe
Omer Ali Adali <omadali@k1m23s07.42kocaeli.com.tr>
2026-05-09 19:59:28 +0300

    fix: Makefile fix for 98 flag and -w -w -w flags

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -13,7 +13,17 @@ int main() {
 	try 
 	{
 		std::cout << a[10] << std::endl;
-	} catch (const std::exception& e) 
+	} 
+	catch (const std::exception& e) 
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	try 
+	{
+		for(int i = 0; i < 5;i++)
+			std::cout << a[i] << std::endl;
+	} 
+	catch (const std::exception& e) 
 	{
 		std::cout << "Exception: " << e.what() << std::endl;
 	}
